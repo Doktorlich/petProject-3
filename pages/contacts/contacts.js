01f1fef0a3c8d14aa2b1703e1e2a3e90bab52ba1
Doktorlich <sergei199522@mail.ru>
2024-05-04 17:19:10 +0300

    A complete skeleton of the site was created, without adaptive
     Changes to be committed:
    	modified:   index.html
    	modified:   pages/contacts/contacts.html
    	modified:   pages/contacts/contacts.js
    	new file:   pages/contacts/main-contacts.scss
    	new file:   pages/new-cars/main-cars.scss
    	modified:   pages/new-cars/new-cars.html
    	modified:   pages/new-cars/new-cars.js
    	new file:   public/images/jpg/blog-img-1.jpeg
    	new file:   public/images/jpg/blog-img-2.jpeg
    	new file:   public/images/jpg/card-img-1.jpeg
    	new file:   public/images/jpg/card-img-2.jpeg
    	new file:   public/images/jpg/card-img-3.jpeg
    	new file:   public/images/jpg/card-img-4.jpeg
    	new file:   public/images/jpg/important-img-1.jpeg
    	new file:   public/images/jpg/important-img-2.jpeg
    	new file:   public/images/jpg/important-img-3.jpeg
    	new file:   public/images/jpg/important-img-4.jpeg
    	new file:   public/images/svg/why-lease-img-1.svg
    	new file:   public/images/svg/why-lease-img-2.svg
    	new file:   public/images/svg/why-lease-img-3.svg
    	new file:   public/images/svg/why-lease-img-4.svg
    	modified:   src/scss/header.scss
    	modified:   src/scss/main/main.scss

--- pages/contacts/contacts.js
+++ pages/contacts/contacts.js
@@ -1,1 +1,1 @@
-
+import "./main-contacts.scss";
--- pages/new-cars/new-cars.js
+++ pages/new-cars/new-cars.js
@@ -1,1 +1,37 @@
+import "./main-cars.scss";
 
+// .tabs__btn-item
+// .tabs__content-item
+//tabs__btn-item--active
+const tabParentItems = document.querySelector(".tabs__btn");
+const tabItems = document.querySelectorAll(".tabs__btn-item");
+const tabContents = document.querySelectorAll(".tabs__content-item");
+
+const hiddenContent = function () {
+    tabItems.forEach(item => {
+        item.classList.remove("tabs__btn-item--active");
+    });
+    tabContents.forEach(item => {
+        item.classList.add("hidden");
+    });
+};
+hiddenContent()
+tabItems[0].classList.add("tabs__btn-item--active");
+tabContents[0].classList.remove("hidden");
+
+const showContent = function (currentItem) {
+    const itemData = currentItem.dataset.button;
+    document.getElementById(itemData).classList.remove("hidden");
+};
+
+tabParentItems.addEventListener("click", e => {
+    const currentItem = e.target;
+
+    if (currentItem.classList.contains("tabs__btn-item")) {
+        hiddenContent();
+        currentItem.classList.add("tabs__btn-item--active");
+        showContent(currentItem);
+    } else {
+        return;
+    }
+});
